4e15578b8994d7fb4ce05683eea6b99a1cfc763b
Oscar James Price <43356609+OscarJPrice@users.noreply.github.com>
2024-01-27 17:58:56 -0800

    Slight update to line counting

--- cntls.sh
+++ cntls.sh
@@ -1,1 +1,1 @@
-find Working -name '*.[ch]*' | xargs wc -l
+find Working \( -name '*.[ch]*' -o -name 'CMakeLists.txt' -o -name '*.cmake' -o -name '*.glsl' -o -name '*.frag' -o -name '*.vert' \) | xargs wc -l
