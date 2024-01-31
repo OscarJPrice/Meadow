7cd66fbba24a5c43cfa69cd050492f5e728a0eac
Oscar James Price <43356609+OscarJPrice@users.noreply.github.com>
2024-01-31 01:04:01 -0800

    Minor Fixes

--- Working/Source/main.cpp
+++ Working/Source/main.cpp
@@ -8,18 +8,16 @@
 #include "Frames.hpp"
 #include "Config.h"
 
-int main(){
+int main() {
 	
 	GraphicsContext gc("Meadow");
 	Swapchain sc(gc);
 	RenderPass rp (gc.getLogicalDevice(), sc.getFormat());
 	sc.setRenderPass(rp);
-		
 	Pipeline p(gc, sc, {
 		Shader::create(SHADER_BINARY_DIR "Shader.vert.spv", gc.getLogicalDevice(), VK_SHADER_STAGE_VERTEX_BIT),
 		Shader::create(SHADER_BINARY_DIR "Shader.frag.spv", gc.getLogicalDevice(), VK_SHADER_STAGE_FRAGMENT_BIT)
 	}, false);
-
 	Frames fif(gc, sc, p);
 
 	while (gc) {
