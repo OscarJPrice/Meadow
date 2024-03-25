6616dba188ea5b275d99cdb5da05a5d93c22f169
Oscar James Price <43356609+OscarJPrice@users.noreply.github.com>
2024-03-24 18:20:33 -0700

    moved files to Source ranther than Working

--- Working/Vulkan/VulkanSwapchainSupportDetails.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <vector>
-#include <vulkan/vulkan.h>
-struct SwapchainSupportDetails {
-  VkSurfaceCapabilitiesKHR capabilities{};
-  std::vector<VkSurfaceFormatKHR> formats;
-  std::vector<VkPresentModeKHR> present_modes;
-};
